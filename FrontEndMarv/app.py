f910755630484e2258ec5d5f75a70440e575476a
Marvin Ojeda <marvinjoseojeda2@gmail.com>
2022-12-04 12:20:26 -0600

    Agregando nueva home de UnsirifaGames

--- FrontEndMarv/app.py
+++ FrontEndMarv/app.py
@@ -6,7 +6,7 @@ app = Flask(__name__) #variable de entorno que contiene el "main"
 @app.route("/") #definimos que hay una ruta de acceso con el arroba
 def index():
     nombre = request.args.get('nombre', 'mundo')
-    return render_template("index.html", nombre = nombre)
+    return render_template("home.html", nombre = nombre)
 
 if __name__ == '__main__':
     app.run()
