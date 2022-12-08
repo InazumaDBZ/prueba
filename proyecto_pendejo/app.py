6433637937d95394d0f3ef1a494d04729e301972
Eliuth <bryan.e.cordero@gmail.com>
2022-12-08 12:46:45 -0600

    agregando juegos de terror

--- proyecto_pendejo/app.py
+++ proyecto_pendejo/app.py
@@ -82,3 +82,68 @@ def lampadaStreet():
 # def geoDash():
 #     return render_template("/casuales/")
 
+#terror-----------------------------------
+
+@app.route("/terror")
+def terror():
+    return render_template("terror.html")
+
+@app.route("/terror/kuzbass-horror")
+def kuzbass_horror():
+    return render_template("/terror/kuzbass-horror.html")
+
+@app.route("/terror/backwoods")
+def backwoods():
+    return render_template("/terror/backwoods.html")
+
+@app.route("/terror/creepy-granny-scream-scary-freddy")
+def creepy_granny_scream_scary_freddy():
+    return render_template("/terror/creepy-granny-scream-scary-freddy.html")
+
+@app.route("/terror/haunted-school---horror-game")
+def haunted_school_horror_game():
+    return render_template("/terror/haunted-school---horror-game.html")
+
+@app.route("/terror/isles-of-mists-horror-story")
+def isles_of_mists_horror_story():
+    return render_template("/terror/isles-of-mists-horror-story.html")
+
+@app.route("/terror/brother-wake-up")
+def brother_wake_up():
+    return render_template("/terror/brother-wake-up.html")
+
+@app.route("/terror/house-of-celestina")
+def house_of_celestina():
+    return render_template("/terror/house-of-celestina.html")
+
+@app.route("/terror/bear-haven-2")
+def bear_haven_2():
+    return render_template("/terror/bear-haven-2.html")
+
+@app.route("/terror/celestina-chapter-two")
+def celestina_chapter_two():
+    return render_template("/terror/celestina-chapter-two.html")
+
+@app.route("/terror/case-smile-horror-game-adventure")
+def case_smile_horror_game_adventure():
+    return render_template("/terror/case-smile-horror-game-adventure.html")
+
+@app.route("/terror/case-smile-2")
+def case_smile_2():
+    return render_template("/terror/case-smile-2.html")
+
+@app.route("/terror/in-to-the-forest")
+def in_to_the_forest():
+    return render_template("/terror/in-to-the-forest.html")
+
+@app.route("/terror/saw-4-trapped")
+def saw_4_trapped():
+    return render_template("/terror/saw-4-trapped.html")
+
+@app.route("/terror/slenderman-must-die-silent-streets")
+def slenderman_must_die_silent_streets():
+    return render_template("/terror/slenderman-must-die-silent-streets.html")
+
+@app.route("/terror/slenderman-must-die-sanatorium-2021")
+def slenderman_must_die_sanatorium_2021():
+    return render_template("/terror/slenderman-must-die-sanatorium-2021.html")
